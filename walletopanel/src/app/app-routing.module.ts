ce07ffe1b154b4a8e91f7a1907cd06fddb7cdc73
Victor Marinho <victor.marinho@hipr.com.br>
2020-02-18 11:50:05 -0300

    init

--- walletopanel/src/app/app-routing.module.ts
+++ walletopanel/src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {LoginComponent} from './components/login/login.component';
+import {InvoiceComponent} from './components/invoice/invoice.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {path: '', component: InvoiceComponent}
+    ]
+
+  },
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {
+      useHash: false,
+      anchorScrolling: 'enabled'
+    }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- walletopanel/src/app/app.module.ts
+++ walletopanel/src/app/app.module.ts
@@ -6,16 +6,30 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HomeComponent} from './components/home/home.component';
+import {LoginComponent} from './components/login/login.component';
+import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
+import { InvoiceComponent } from './components/invoice/invoice.component';
+import { DragDropDirective } from './shared/directives/DragDropDirective/drag-drop.directive';
+import { UploadComponent } from './shared/upload/upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    HomeComponent,
+    MenuBarComponent,
+    InvoiceComponent,
+    UploadComponent,
   ],
   imports: [
     CoreModule,
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
